3e3760b96aa3ed50144d8b7d7c43ec94a5e5efe0
saradonin <m.jemieljanczuk@gmail.com>
2023-06-18 23:46:39 +0200

    AddRoom view skeleton created

--- reservations/views.py
+++ reservations/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views import View
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+class AddRoom(View):
+    def get(self, request):
+        return render(request, 'add_room.html')
+    def post(self, request):
+        return redirect('/home/')
