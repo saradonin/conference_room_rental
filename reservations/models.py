d9a40e1fe120a0302c6f38f4d586a90e56618879
saradonin <m.jemieljanczuk@gmail.com>
2023-06-19 23:07:29 +0200

    models Reservation created

--- reservations/models.py
+++ reservations/models.py
@@ -5,3 +5,13 @@ class Room(models.Model):
     name = models.CharField(max_length=255, unique=True)
     capacity = models.PositiveIntegerField()
     projector_availability = models.BooleanField(default=False)
+
+
+class Reservation(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    date = models.DateField()
+    comment = models.TextField(null=True)
+
+    class Meta:
+        unique_together = ('room', 'date')
+
