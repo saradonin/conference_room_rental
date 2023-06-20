7a66bd8d9bdf511ebbec8b5746d4d1351b4edcee
saradonin <m.jemieljanczuk@gmail.com>
2023-06-20 23:32:40 +0200

    dosctrings added to models

--- reservations/models.py
+++ reservations/models.py
@@ -2,16 +2,21 @@ from django.db import models
 
 
 class Room(models.Model):
+    """
+    Represents a room with a name, capacity, and projector availability.
+    """
     name = models.CharField(max_length=255, unique=True)
     capacity = models.PositiveIntegerField()
     projector_availability = models.BooleanField(default=False)
 
 
 class Reservation(models.Model):
+    """
+    Represents a reservation made for a specific room on a specific date.
+    """
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     date = models.DateField()
     comment = models.TextField(null=True)
 
     class Meta:
         unique_together = ('room', 'date')
-
